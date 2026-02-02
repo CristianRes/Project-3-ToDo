4235d3632e5702a764b49666505061b2471fdd25
CristianRes <cristianrez07@gmail.com>
2026-02-02 14:38:18 -0600

    Setting up Forms

--- src/Components/Pages/ContactPage.jsx
+++ src/Components/Pages/ContactPage.jsx
@@ -1,8 +1,29 @@
+import Form from 'react-bootstrap/Form';
+
+
 function ContactPage(){
+
+    function handleChange(){
+    }
+
     return(
         <>
-            <h1>Contact Us</h1>
-            <p> Details of Contact</p>
+        <h1>Contact Us</h1>
+        <p> Details of Contact</p>
+        <Form>
+            
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control 
+                type="text" 
+                name="FirstName"
+                value={formData.FirstName}
+                onChange={handleChange} 
+                />
+
+            </Form.Group>
+        </Form>
+            
         </>
     ) 
 }
